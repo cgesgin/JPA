9d128d8be84b7eb297439792cbca005489c3c851
cgesgin <cevher-5@hotmail.com>
2021-07-09 12:31:11 +0300

    servlet üzerinde jpa uygulandı

---
+++ 03_JPA_SERVLET/src/entity/Employee.java
@@ -0,0 +1,68 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID")
+	private int id;
+	
+	@Column(name="NAME")
+	private String name;
+	
+	@Column(name="SURNAME")
+	private String surname;
+	
+	@Column(name="SALARY")
+	private int salary;
+
+	public Employee() {
+		
+	}
+
+	public Employee(String name, String surname, int salary) {
+		this.name = name;
+		this.surname = surname;
+		this.salary = salary;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+}
